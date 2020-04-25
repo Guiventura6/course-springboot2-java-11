bc5bbedddbe345c338e7b94dac6cd64d90aeacf9
Guilherme Ventura sousa <61128802+GuilhermeVenturaSousa@users.noreply.github.com>
2020-04-25 15:59:55 -0300

    Payment, one-to-one association

--- src/main/java/com/guiconsultoria/course/config/TestConfig.java
+++ src/main/java/com/guiconsultoria/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.guiconsultoria.course.entities.Category;
 import com.guiconsultoria.course.entities.Order;
 import com.guiconsultoria.course.entities.OrderItem;
+import com.guiconsultoria.course.entities.Payment;
 import com.guiconsultoria.course.entities.Product;
 import com.guiconsultoria.course.entities.User;
 import com.guiconsultoria.course.entities.enums.OrderStatus;
@@ -81,5 +82,9 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
