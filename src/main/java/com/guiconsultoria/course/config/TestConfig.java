02b67dbe8d331309f8b43086d607c3f8303a9a7f
Guilherme Ventura sousa <61128802+GuilhermeVenturaSousa@users.noreply.github.com>
2020-04-25 14:44:52 -0300

    OrderItem, many-to-many association with extra attributes - Part 2

--- src/main/java/com/guiconsultoria/course/config/TestConfig.java
+++ src/main/java/com/guiconsultoria/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.guiconsultoria.course.entities.Category;
 import com.guiconsultoria.course.entities.Order;
+import com.guiconsultoria.course.entities.OrderItem;
 import com.guiconsultoria.course.entities.Product;
 import com.guiconsultoria.course.entities.User;
 import com.guiconsultoria.course.entities.enums.OrderStatus;
 import com.guiconsultoria.course.repositories.CategoryRepository;
+import com.guiconsultoria.course.repositories.OrderItemRepository;
 import com.guiconsultoria.course.repositories.OrderRepository;
 import com.guiconsultoria.course.repositories.ProductRepository;
 import com.guiconsultoria.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -56,9 +61,9 @@ public class TestConfig implements CommandLineRunner {
 		p3.getCategories().add(cat3);
 		p4.getCategories().add(cat3);
 		p5.getCategories().add(cat2);
-		
+
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-		
+
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
@@ -69,5 +74,12 @@ public class TestConfig implements CommandLineRunner {
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
 	}
 }
---
+++ src/main/java/com/guiconsultoria/course/repositories/OrderItemRepository.java
@@ -0,0 +1,11 @@
+package com.guiconsultoria.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.guiconsultoria.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+
+	
+	
+}
